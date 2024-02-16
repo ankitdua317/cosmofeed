9e13b9485e3a3ac65a460b2b81e5a5d7185686ef
Ankit Dua <ankitdua317@gmail.com>
2024-02-16 16:17:26 +0530

    Fix home page api call

--- src/api/fetchHomePageData.ts
+++ src/api/fetchHomePageData.ts
@@ -1,12 +1,13 @@
 import { get } from ".";
-import { Data } from "@/models/Home";
+import { LayoutAPIResponse } from "@/models/Home";
 import DATA from "@/constants/home";
 
 const fetchHomePageData = async () => {
   try {
-    return await get<Data>(
+    const { data } = await get<LayoutAPIResponse>(
       "https://run.mocky.io/v3/48f264ae-db1c-402b-987f-8e9ee653d10b"
     );
+    return data;
   } catch (err) {
     return DATA;
   }
